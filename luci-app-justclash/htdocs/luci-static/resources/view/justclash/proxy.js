047a7f050c00f78c83dced2ec5c0de637ee9136a
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2025-06-15 14:56:56 +0300

    proxy refactor

--- luci-app-justclash/htdocs/luci-static/resources/view/justclash/proxy.js
+++ luci-app-justclash/htdocs/luci-static/resources/view/justclash/proxy.js
@@ -9,15 +9,15 @@
 
 return view.extend({
 
-    load: function() {
-       return Promise.all([
-            uci.load("justclash"),
+    load: function () {
+        return Promise.all([
+            uci.load(common.binName),
         ]).catch(e => {
-            ui.addNotification(null, E("p", _("Unable to read the contents") + ": %s ".format(e.message) ));
+            ui.addNotification(null, E("p", _("Unable to read the contents") + ": %s ".format(e.message)));
         });
     },
     render: function () {
-         let m, s, o, tabname;
+        let m, s, o, tabname;
 
         m = new form.Map(common.binName);
 
@@ -26,7 +26,7 @@ return view.extend({
         tabname = "coresettings_tab";
         s.tab(tabname, "Core settings");
 
-        o = s.taboption(tabname, form.ListValue, "log_level", _("Logging level:"))
+        o = s.taboption(tabname, form.ListValue, "log_level", _("Logging level:"));
         common.defaultLoggingLevels.forEach(item => {
             o.value(item, _(`${item}`));
         });
@@ -41,124 +41,161 @@ return view.extend({
 
         o = s.taboption(tabname, form.Flag, "unified_delay", _("Unified delay:"));
         o.description = _("Unified delay for RTT checks.");
-        o.default = 1;
+        o.default = "1";
         o.rmempty = false;
 
         o = s.taboption(tabname, form.Value, "routing_mark", _("Routing mark:"));
         o.description = _("Special mark for internal routing tables.");
         o.datatype = "uinteger";
         o.rmempty = false;
+        o.default = "9";
 
         o = s.taboption(tabname, form.Flag, "tcp_concurrent", _("TCP concurrent:"));
-        o.description = _("tcp_concurrent_description")
+        o.description = _("tcp_concurrent_description");
         o.rmempty = false;
-        o.default = 1;
+        o.default = "1";
 
         o = s.taboption(tabname, form.Value, "external_controller_port", _("External controller port:"));
-        o.description = _("API server port.")
+        o.description = _("API server port.");
         o.datatype = "uinteger";
         o.rmempty = false;
 
-        o = s.taboption(tabname, form.ListValue, "global_client_fingerprint", _("Global client fingerprint:"))
+        o = s.taboption(tabname, form.ListValue, "global_client_fingerprint", _("Global client fingerprint:"));
         common.defaultFingerprints.forEach(item => {
             o.value(item, _(`${item}`));
         });
+
         o.description = _("Client fingerprint for protols which supports it.");
-        o.default = "chrome";
+        o.default = common.defaultFingerprints[0];
         o.rmempty = false;
 
         o = s.taboption(tabname, form.Value, "keep_alive_idle", _("Keep alive idle:"));
-        o.description = _("keep_alive_idle_description")
+        o.description = _("keep_alive_idle_description");
         o.datatype = "uinteger";
         o.rmempty = false;
+        o.default = "15";
 
         o = s.taboption(tabname, form.Value, "keep_alive_interval", _("Keep alive interval:"));
-        o.description = _("keep_alive_interval_description")
+        o.description = _("keep_alive_interval_description");
         o.datatype = "uinteger";
         o.rmempty = false;
+        o.default = "15";
 
         o = s.taboption(tabname, form.Flag, "profile_store_selected", _("profile_store_selected"));
-        o.description = _("profile_store_selected_description")
+        o.description = _("profile_store_selected_description");
         o.rmempty = false;
-        o.default = 1;
+        o.default = "1";
 
         o = s.taboption(tabname, form.Flag, "profile_store_fake_ip", _("profile_store_fake_ip"));
-        o.description = _("profile_store_fake_ip_description")
+        o.description = _("profile_store_fake_ip_description");
         o.rmempty = false;
-        o.default = 1;
+        o.default = "1";
 
         o = s.taboption(tabname, form.DynamicList, "ignore_to_sniff_domains", _("Skip analyse for domains:"));
-        o.description = _("ignore_to_sniff_domains_description")
+        o.description = _("ignore_to_sniff_domains_description");
         o.rmempty = false;
+        o.editable = true;
 
         tabname = "ntpsettings_tab";
         s.tab(tabname, "NTP settings");
 
         o = s.taboption(tabname, form.Flag, "core_ntp_enabled", _("Enable NTP client:"));
-        o.description = _("core_ntp_enabled_description")
+        o.description = _("core_ntp_enabled_description");
         o.rmempty = false;
-        o.default = 1;
+        o.default = "1";
 
         o = s.taboption(tabname, form.Value, "core_ntp_server", _("Endpoint NTP server:"));
-        o.description = _("core_ntp_server_description")
+        o.description = _("core_ntp_server_description");
         o.datatype = "ipaddr";
         o.rmempty = false;
 
         o = s.taboption(tabname, form.Value, "core_ntp_port", _("NTP port:"));
-        o.description = _("core_ntp_port_description")
+        o.description = _("core_ntp_port_description");
         o.datatype = "port";
         o.rmempty = false;
 
         o = s.taboption(tabname, form.Value, "core_ntp_interval", _("Check interval"));
-        o.description = _("core_ntp_interval_description")
+        o.description = _("core_ntp_interval_description");
         o.datatype = "uinteger";
         o.rmempty = false;
+        o.default = "600";
 
         o = s.taboption(tabname, form.Flag, "core_ntp_write_system", _("Write to system:"));
-        o.description = _("core_ntp_write_system_description")
-        o.default = 0;
+        o.description = _("core_ntp_write_system_description");
+        o.default = "0";
         o.rmempty = false;
 
         tabname = "dnssettings_tab";
         s.tab(tabname, "DNS settings");
 
         o = s.taboption(tabname, form.Value, "dns_listen_port", _("DNS listen port:"));
-        o.description = _("dns_listen_port_description")
+        o.description = _("dns_listen_port_description");
         o.datatype = "port";
         o.rmempty = false;
 
         o = s.taboption(tabname, form.Flag, "use_system_hosts", _("Use system hosts:"));
-        o.description = _("use_system_hosts_description")
+        o.description = _("use_system_hosts_description");
         o.rmempty = false;
-        o.default = 1;
+        o.default = "1";
 
         o = s.taboption(tabname, form.Value, "fake_ip_range", _("Fake IP range:"));
-        o.description = _("fake_ip_range_description")
-        o.default = "198.18.0.1/16"
+        o.description = _("fake_ip_range_description");
+        o.default = "198.18.0.1/16";
         o.rmempty = false;
+        o.datatype = "cidr4";
 
         o = s.taboption(tabname, form.DynamicList, "default_nameserver", _("Default nameservers:"));
-        o.description = _("default_nameserver_description")
+        o.description = _("default_nameserver_description");
         o.rmempty = false;
+        o.editable = true;
+        o.validate = function (section_id, value) {
+            if ((!common.isValidIpv4(value)) || (!common.isValidDomainProto(value)))
+                return _("Invalid nameserver format. Allowed: quick://, https://, tls://, udp://, IPv4 or 'system'.");
 
+            return true;
+        };
         o = s.taboption(tabname, form.DynamicList, "direct_nameserver", _("Direct nameservers:"));
-        o.description = _("direct_nameserver_description")
+        o.description = _("direct_nameserver_description");
         o.rmempty = false;
+        o.editable = true;
+        o.validate = function (section_id, value) {
+            if ((!common.isValidIpv4(value)) || (!common.isValidDomainProto(value)))
+                return _("Invalid nameserver format. Allowed: quick://, https://, tls://, udp://, IPv4 or 'system'.");
 
+            return true;
+        };
         o = s.taboption(tabname, form.DynamicList, "proxy_server_nameserver", _("Proxy nameservers:"));
-        o.description = _("proxy_server_nameserver_description")
+        o.description = _("proxy_server_nameserver_description");
         o.rmempty = false;
+        o.editable = true;
+        o.validate = function (section_id, value) {
+            if ((!common.isValidIpv4(value)) || (!common.isValidDomainProto(value)))
+                return _("Invalid nameserver format. Allowed: quick://, https://, tls://, udp://, IPv4 or 'system'.");
 
+            return true;
+        };
         o = s.taboption(tabname, form.DynamicList, "nameserver", _("Nameservers:"));
-        o.description = _("nameserver_description")
+        o.description = _("nameserver_description");
         o.rmempty = false;
+        o.editable = true;
+        o.validate = function (section_id, value) {
+            if ((!common.isValidIpv4(value)) || (!common.isValidDomainProto(value)))
+                return _("Invalid nameserver format. Allowed: quick://, https://, tls://, udp://, IPv4 or 'system'.");
 
+            return true;
+        };
         o = s.taboption(tabname, form.DynamicList, "ignore_fake_ip_domains", _("Skip fake IP for domains:"));
-        o.description = _("ignore_fake_ip_domains_description")
+        o.description = _("ignore_fake_ip_domains_description");
         o.rmempty = false;
+        o.editable = true;
 
         let map_promise = m.render();
         return map_promise;
+    },
+    addCSS() {
+        return E("style", {}, `
+
+        `);
     },
     destroy() {
 
