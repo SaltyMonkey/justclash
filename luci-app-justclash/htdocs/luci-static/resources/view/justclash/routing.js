40709bf242b5e9a09cb9a754afeabb3a6ab8bc3b
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2025-06-16 04:30:09 +0300

    refactor, messages

--- luci-app-justclash/htdocs/luci-static/resources/view/justclash/routing.js
+++ luci-app-justclash/htdocs/luci-static/resources/view/justclash/routing.js
@@ -24,39 +24,41 @@ return view.extend({
                 let virtualProxies = [];
                 let virtualProxyGroups = [];
                 let virtualRules = "";
-
+                let virtualBlockRules = "";
                 let addedProxyNames = [];
 
                 for (const s of allSections) {
                     const type = s[".type"];
-                    const name = s.name;
+                    const name = s.name.trim();
                     switch (type) {
                         case "proxies":
-                            const link = s.proxy_link;
+                            const link = s.proxy_link.trim();;
 
                             let obj = null;
                             if (link) {
                                 try {
-                                    obj = common.parseVlessLink(link);
+                                    obj = common.parseProxyLink(link);
                                     obj.name = name;
                                     virtualProxies.push(obj);
                                     addedProxyNames.push(name);
                                 } catch (e) {
-                                    console.error("Ошибка парсинга proxy_link:", e);
+                                    console.error(_("Cannot parse proxy URI:"), e);
                                 }
                             }
                             if (obj) {
-                                const selectedRuleSetsNames = common.valueToArray(s.enabled_blocklist);
+                                const selectedRuleSetsNames = common.valueToArray(s.enabled_list);
                                 const domainRoutes = common.valueToArray(s.additional_domain_route);
                                 const destipRoutes = common.valueToArray(s.additional_destip_route);
-                                selectedRuleSetsNames.forEach(name => {
-                                    const rs = rulesets.availableRuleSets.find(x => name === x.yamlName);
+                                selectedRuleSetsNames.forEach(ruleset => {
+                                    const rs = rulesets.availableRuleSets.find(x => ruleset === x.yamlName);
                                     if (rs) {
                                         let copy = Object.assign({}, rs);
                                         const yamlName = copy.yamlName;
                                         delete copy.name;
                                         delete copy.yamlName;
                                         virtualRuleSets[yamlName] = copy;
+
+                                        virtualRules += ` - RULE-SET,${yamlName},${name}\n`;
                                     }
                                 });
                                 domainRoutes.forEach(domain => {
@@ -71,7 +73,7 @@ return view.extend({
                         case "proxy_group":
                             let proxyList = null;
                             if (s.proxies_list) proxyList = common.splitAndTrimString(s.proxies_list, ",");
-                            if (proxyList.length > 1) {
+                            if (proxyList && proxyList.length > 1) {
                                 virtualProxyGroups.push({
                                     name,
                                     type: s.group_type,
@@ -82,29 +84,55 @@ return view.extend({
                                     proxies: proxyList,
                                     lazy: false
                                 });
-                                const selectedRuleSetsNames = common.valueToArray(s.enabled_blocklist);
+                                const selectedRuleSetsNames = common.valueToArray(s.enabled_list);
                                 const domainRoutes = common.valueToArray(s.additional_domain_route);
                                 const destipRoutes = common.valueToArray(s.additional_destip_route);
-                                selectedRuleSetsNames.forEach(name => {
-                                    const rs = rulesets.availableRuleSets.find(x => name === x.yamlName);
+                                selectedRuleSetsNames.forEach(ruleset => {
+                                    const rs = rulesets.availableRuleSets.find(x => ruleset === x.yamlName);
                                     if (rs) {
                                         let copy = Object.assign({}, rs);
                                         const yamlName = copy.yamlName;
                                         delete copy.name;
                                         delete copy.yamlName;
                                         virtualRuleSets[yamlName] = copy;
+
+                                        virtualRules += ` - RULE-SET,${yamlName},${name}\n`;
                                     }
                                 });
                                 domainRoutes.forEach(domain => {
                                     virtualRules += ` - DOMAIN-SUFFIX,${domain},${name}\n`;
                                 });
 
-                                destipRoutes.forEach(domain => {
-                                    virtualRules += ` - IP-CIDR,${domain},${name}\n`;
+                                destipRoutes.forEach(cidr => {
+                                    virtualRules += ` - IP-CIDR,${cidr},${name}\n`;
                                 });
                             }
                             break;
+                        case "block_rules":
+                            const selectedBlockRuleSetsNames = common.valueToArray(s.enabled_blocklist);
+                            const domainBlockRoutes = common.valueToArray(s.additional_domain_blockroute);
+                            const destipBlockRoutes = common.valueToArray(s.additional_destip_blockroute);
+                            const disableQuick = s.disable_quic;
+                            selectedBlockRuleSetsNames.forEach(ruleset => {
+                                const rs = rulesets.availableBlockRulesets.find(x => ruleset === x.yamlName);
+                                if (rs) {
+                                    let copy = Object.assign({}, rs);
+                                    const yamlName = copy.yamlName;
+                                    delete copy.name;
+                                    delete copy.yamlName;
+                                    virtualRuleSets[yamlName] = copy;
 
+                                    virtualBlockRules += ` - RULE-SET,${yamlName},REJECT\n`;
+                                }
+                            });
+                            domainBlockRoutes.forEach(domain => {
+                                virtualBlockRules += ` - DOMAIN-SUFFIX,${domain},REJECT\n`;
+                            });
+
+                            destipBlockRoutes.forEach(cidr => {
+                                virtualBlockRules += ` - IP-CIDR,${cidr},REJECT\n`;
+                            });
+                            break;
                         default:
                             // Handle unknown types if needed
                             break;
@@ -112,11 +140,12 @@ return view.extend({
                 }
 
                 console.log(virtualRules);
+                console.log(virtualBlockRules);
                 console.log(common.objToYaml(virtualProxies, 1));
                 console.log(common.objToYaml(virtualProxyGroups, 1));
                 console.log(common.objToYaml(virtualRuleSets, 1));
 
-                uci.set(common.binName, "compiled", "rules", virtualRules);
+                uci.set(common.binName, "compiled", "rules", `${virtualBlockRules}${virtualRules}`);
                 uci.set(common.binName, "compiled", "proxies", common.objToYaml(virtualProxies, 1));
                 uci.set(common.binName, "compiled", "proxy_groups", common.objToYaml(virtualProxyGroups, 1));
                 uci.set(common.binName, "compiled", "rule_providers", common.objToYaml(virtualRuleSets, 1));
@@ -133,12 +162,12 @@ return view.extend({
         let m, s, s2, s3, o;
 
         m = new form.Map(common.binName);
-        s = m.section(form.TypedSection, "proxies", _("Proxies"), _("description"));
+        s = m.section(form.TypedSection, "proxies", _("Proxies"), _("Proxies defined as outbounds"));
         s.anonymous = true;
         s.addremove = true;
 
-        o = s.option(form.Value, "name", _("name:"));
-        o.description = _("name_description");
+        o = s.option(form.Value, "name", _("Name:"));
+        o.description = _("Proxy name.");
         o.rmempty = false;
         o.cfgvalue = function (section_id) {
             const val = uci.get(common.binName, section_id, "name");
@@ -150,33 +179,34 @@ return view.extend({
             return (common.isValidSimpleName(value)) ? true : _("Name must contain only lowercase letters, digits, and underscores");
         };
 
-        o = s.option(form.Value, "proxy_link", _("proxy_link:"));
-        o.description = _("proxy_link_description");
+        o = s.option(form.Value, "proxy_link", _("Proxy URI:"));
+        o.description = _("URI link with proxy data.");
+        o.password = true;
         o.validate = function (section_id, value) {
             return (common.isValidProxyLink(value)) ? true : _("Invalid link.");
         };
 
-        o = s.option(form.DynamicList, "enabled_list", _("enabled_list:"));
+        o = s.option(form.MultiValue, "enabled_list", _("Use for rules:"));
         rulesets.availableRuleSets.forEach(item => {
             o.value(item.yamlName, _(`${item.name}`));
         });
         o.optional = true;
 
-        o = s.option(form.DynamicList, "additional_domain_route", _("additional_domain_route:"));
-        o.description = _("additional_domain_route_description");
+        o = s.option(form.DynamicList, "additional_domain_route", _("Manual DOMAIN-SUFFIX rules:"));
+        o.description = _("One element is one DOMAIN-SUFFIX rule with mihomo syntax.");
         o.optional = true;
 
-        o = s.option(form.DynamicList, "additional_destip_route", _("additional_destip_route:"));
-        o.description = _("additional_destip_route_description");
+        o = s.option(form.DynamicList, "additional_destip_route", _("Manual IP-CIDR rules:"));
+        o.description = _("One element is one IP-CIDR rule with mihomo syntax. IPV4 only right now.");
         o.optional = true;
         o.datatype = "cidr4";
 
-        s2 = m.section(form.TypedSection, "proxy_group", _("Proxy groups"), _("proxy groups description"));
+        s2 = m.section(form.TypedSection, "proxy_group", _("Proxy groups:"), _("Proxy groups for defined proxies"));
         s2.anonymous = true;
         s2.addremove = true;
 
-        o = s2.option(form.Value, "name", _("name:"));
-        o.description = _("name_description");
+        o = s2.option(form.Value, "name", _("Name:"));
+        o.description = _("Proxy group name.");
         o.rmempty = false;
         o.cfgvalue = function (section_id) {
             const val = uci.get(common.binName, section_id, "name");
@@ -188,24 +218,24 @@ return view.extend({
             return (common.isValidSimpleName(value)) ? true : _("Name must contain only lowercase letters, digits, and underscores");
         };
 
-        o = s2.option(form.ListValue, "group_type", _("group_type:"));
+        o = s2.option(form.ListValue, "group_type", _("Group type:"));
         common.defaultProxyGroupsTypes.forEach(item => {
-            o.value(items, _(`${item}`));
+            o.value(item, _(`${item}`));
         });
         o.rmempty = false;
         o.default = common.defaultProxyGroupsTypes[0];
 
-        o = s2.option(form.ListValue, "strategy", _("strategy:"));
+        o = s2.option(form.ListValue, "strategy", _("Group strategy:"));
         common.defaultProxyGroupsBalanceModeStrategies.forEach(item => {
             o.value(item, _(`${item}`));
         });
         o.default = common.defaultProxyGroupsBalanceModeStrategies[0];
         o.depends("group_type", "load-balancer");
 
-        o = s2.option(form.Value, "proxies_list", _("proxies_list:"));
+        o = s2.option(form.Value, "proxies_list", _("Proxies:"));
         o.placeholder = "proxy-name1, proxy-name2";
 
-        o = s2.option(form.Value, "check_url", _("checkUrl:"));
+        o = s2.option(form.Value, "check_url", _("Check URL:"));
         o.placeholder = common.defaultProxyGroupCheckUrl;
         o.default = common.defaultProxyGroupCheckUrl;
         o.rmempty = false;
@@ -213,56 +243,46 @@ return view.extend({
             return (common.isValidHttpUrl(value)) ? true : _("Only http:// or https:// URLs are allowed.");
         };
 
-        o = s2.option(form.Value, "interval", _("interval:"));
+        o = s2.option(form.Value, "interval", _("Check interval:"));
         o.datatype = "uinteger";
         o.default = common.defaultProxyGroupInterval;
         o.rmempty = false;
 
-        o = s2.option(form.DynamicList, "enabled_list", _("enabled_list:"));
-        o.multiple = true;
+        o = s2.option(form.MultiValue, "enabled_list", _("Use for rules:"));
         rulesets.availableRuleSets.forEach(item => {
             o.value(item.yamlName, _(`${item.name}`));
         });
 
-        o = s2.option(form.DynamicList, "additional_domain_route", _("additional_domain_route:"));
-        o.description = _("additional_domain_route_description");
+        o = s2.option(form.DynamicList, "additional_domain_route", _("Manual DOMAIN-SUFFIX rules:"));
+        o.description = _("One element is one DOMAIN-SUFFIX rule with mihomo syntax.");
         o.optional = true;
         o.editable = true;
 
-        o = s2.option(form.DynamicList, "additional_destip_route", _("additional_destip_route:"));
-        o.description = _("additional_destip_route_description");
+        o = s2.option(form.DynamicList, "additional_destip_route", _("Manual IP-CIDR rules:"));
+        o.description = _("One element is one IP-CIDR rule with mihomo syntax. IPV4 only right now.");
         o.optional = true;
         o.editable = true;
         o.datatype = "cidr4";
 
-        s3 = m.section(form.NamedSection, "block_rules", _("Blocks"), _("blocks descriptions"));
+        s3 = m.section(form.NamedSection, "block_rules", _("REJECT rules"), _("Additional rules for REJECT rules. Will be handled first."));
         s3.addremove = false;
 
-        o = s3.option(form.Flag, "disable_quic", _("disable_quic:"));
-        o.description = _("disable_quic_description.");
+        o = s3.option(form.Flag, "disable_quic", _("Disable QUIC:"));
+        //o.description = _("disable_quic_description.");
         o.default = "0";
 
-        o = s3.option(form.Flag, "disable_doh", _("disable_doh:"));
-        o.description = _("disable_doh_description.");
-        o.default = "0";
-
-        o = s3.option(form.Flag, "disable_ntp", _("disable_ntp:"));
-        o.description = _("disable_ntp.");
-        o.default = "0";
-
-        o = s3.option(form.DynamicList, "enabled_blocklist ", _("enabled_blocklist :"));
-        o.multiple = true;
+        o = s3.option(form.MultiValue, "enabled_blocklist ", _("Use blocklists:"));
         rulesets.availableBlockRulesets.forEach(item => {
             o.value(item.yamlName, _(`${item.name}`));
         });
 
-        o = s3.option(form.DynamicList, "additional_domain_blockroute", _("additional_domain_blockroute:"));
-        o.description = _("additional_domain_blockroute_description");
+        o = s3.option(form.DynamicList, "additional_domain_blockroute", _("Manual DOMAIN-SUFFIX block rules:"));
+        o.description = _("One element is one DOMAIN-SUFFIX rule with mihomo syntax.");
         o.optional = true;
         o.editable = true;
 
-        o = s3.option(form.DynamicList, "additional_destip_blockroute", _("additional_destip_blockroute:"));
-        o.description = _("additional_destip_blockroute_description");
+        o = s3.option(form.DynamicList, "additional_destip_blockroute", _("Manual IP-CIDR block rules:"));
+        o.description = _("One element is one IP-CIDR rule with mihomo syntax. IPV4 only right now.");
         o.optional = true;
         o.editable = true;
         o.datatype = "cidr4";
