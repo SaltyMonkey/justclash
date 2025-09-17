72cca98e32e2a0a18eb17f52ae4626cfe3f08045
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2025-09-17 11:28:44 +0300

    Service: make migration placeholder externally

---
+++ justclash/files/usr/lib/justclash/migration.sh
@@ -0,0 +1,12 @@
+#!/bin/ash
+# Ash isn't supported properly in spellcheck static analyzer
+# Using debian based version (kind of similar)
+# shellcheck shell=dash
+
+# --------------------------------------------
+# External justclash service part
+# --------------------------------------------
+
+migration() {
+    return 0
+}
