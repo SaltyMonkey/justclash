72fabd8247455799278cc21bf0f98b032c4b0c3c
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2025-12-27 18:27:28 +0300

    Service: skip br_netfilter load check for compat fixes

--- justclash/files/usr/lib/justclash/compat.sh
+++ justclash/files/usr/lib/justclash/compat.sh
@@ -8,17 +8,12 @@
 # --------------------------------------------------------
 
 compat_fixes() {
-   friendlyWRT_fix
+   iptables_fix
 }
 
-friendlyWRT_fix() {
-     if ! command -v lsmod >/dev/null 2>&1; then
-        return 1
-    else
-        if lsmod | grep -qF br_netfilter && [ "$(sysctl -n net.bridge.bridge-nf-call-iptables 2>/dev/null)" = "1" ]; then
-            sysctl -w net.bridge.bridge-nf-call-iptables=0
-            sysctl -w net.bridge.bridge-nf-call-ip6tables=0
-            return 0
-        fi
-    fi
+iptables_fix() {
+    command -v sysctl >/dev/null 2>&1 || return 0
+
+    [ "$(sysctl -n net.bridge.bridge-nf-call-iptables 2>/dev/null)" = "1" ] &&  sysctl -w net.bridge.bridge-nf-call-iptables=0
+    [ "$(sysctl -n net.bridge.bridge-nf-call-ip6tables 2>/dev/null)" = "1" ] &&  sysctl -w net.bridge.bridge-nf-call-ip6tables=0
 }
